d50f1aa380aa280b07b460fcaa754776fa67135c
Marcos <marcosurielmachuca49@gmail.com>
2026-03-02 01:01:42 -0300

    03_Strings.py

---
+++ 03_Strings.py
@@ -0,0 +1,42 @@
+#1
+text = 'Aprendiendo Python'
+print(len(text))
+
+#2
+print('Hola '+ 'Python')
+
+#3
+print('Hola estoy \naprendiendo python')
+
+#4
+name = 'Marcos'
+surname = 'Machuca'
+age = 21
+print(f'Hola me llamo {name} {surname} y tengo {age}')
+
+#5
+palabra = 'Python'
+a,b,c,d,e,f = palabra
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+
+#6
+palabra2 = 'programacion'
+palabra_slice= palabra2[3:7]
+print(palabra_slice)
+
+#7
+print(palabra[:: -1])
+
+#8
+print(text.upper())
+
+#9
+print('Programacion en phyton'.count('n'))
+
+#10
+print("13254".isnumeric())
