c89aada7799cd621ce7d85adffd70987fd58b0fd
Marcos <marcosurielmachuca49@gmail.com>
2026-03-02 00:59:04 -0300

    01_Variables,py

---
+++ 01_Variables.py
@@ -0,0 +1,45 @@
+#1
+name = 'Marcos'
+age = 21
+height = 1.68
+
+print(name)
+print(age)
+print(height)
+
+#2
+print('Tienes: ', str(age))
+
+#3
+is_student = True
+
+#4
+name_completo = len(name)
+
+#5
+name, surname, city= 'Marcos', 'Machuca', 'Buenos Aires'
+print('Mi nombre es: ', name, surname,' Vivo en: ', city)
+
+#6
+color = input('Tu color favorito: ')
+print(color)
+
+#7
+fruit = 15
+print(fruit)
+fruit = 25
+print(fruit)
+
+#8
+price = 9.99
+print(int(price))
+
+#9
+address_len = len('marcosurielmachuca49@gmail.com')
+print(address_len)
+
+#10
+phone: int= 1164898358
+phone = 1.52
+print(type(phone))
+
