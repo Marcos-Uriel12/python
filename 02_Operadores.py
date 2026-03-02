123fa506c849ba561e27691694bc2ee3ef929f41
Marcos <marcosurielmachuca49@gmail.com>
2026-03-02 01:01:32 -0300

    02_Operadores.py

---
+++ 02_Operadores.py
@@ -0,0 +1,38 @@
+#1
+Suma= 15 + 25
+Resta= 50 - 22
+Multiplicación= 8 * 7
+División= 100 / 20
+
+#2
+resto = 37 % 5
+print(resto)
+
+#3
+print('este es mi numero favorito: '+ str(7))
+
+#4
+print('Python' * 10)
+
+#5
+a, b= 12, 8 
+comp = a > b
+print(comp)
+
+#6
+print('apple' > 'banana')
+print('apple' < 'banana')
+
+#7
+print(10 > 5 and 10 < 20)
+
+#8
+print(7 < 3 or 7 > 5  )
+
+#9 
+print(not(15 >20))
+
+#10
+print((((5 * 3) + 2) > 10) < 20 )  
+
+print(10 % 3 + 4 ** 2)
