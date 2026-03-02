8bf38374193098683ffac4989a7552e36fa1ba97
Marcos <marcosurielmachuca49@gmail.com>
2026-03-02 00:58:25 -0300

    00_Hello.py

---
+++ 00_Hello.py
@@ -0,0 +1,42 @@
+print ("hola mundo")
+# Este codigo muestra por consola hola mundo 
+
+#3
+print('Me llamo Marcos, tengo 21')
+
+#4
+print(type('hola')) 
+print(type('2')) 
+print(type('1.5'))
+
+#5
+''' 
+los tipos de datos en python son
+str
+int
+float
+
+'''
+#6
+print('Hola '+' mundo')
+nombre = 'Marcos'
+edad = str(21)
+print('Mi nombre es ' + nombre + ' tengo ' + edad)
+
+#7
+nombre2 = input('Cual es tu nombre: ')
+edad2 = input('cual es tu edad: ') 
+print ('Hola ' + nombre2 + ' tenes ' + edad2)
+
+#8
+nombre3 = input('Cual es tu nombre: ')
+print('Hola '+ nombre3 + ' bienvenido')
+
+#9
+num = 2
+num1 = 3
+sum = num + num1
+print('La suma es: '+ str(sum))
+
+#10
+#El ejercicio 9 obtiene la suma de dos numeros dados y la muestra por pantalla
