7500b89484ab1e1ea3914bd7950ff0269eb8f805
Marcos <marcosurielmachuca49@gmail.com>
2026-03-03 16:49:44 -0300

    04_list.py

---
+++ 04_List.py
@@ -0,0 +1,41 @@
+#1
+new_list = [1,2,3,4,5]
+print(new_list)
+
+#2
+list2 = [10,20,30,30,40,50]
+print(list2[2])
+
+#3
+new_list.append(6)
+print(new_list)
+
+#4
+list2.insert(1,15)
+print(list2)
+
+#5
+list2.remove(30)
+print(list2)
+
+#6
+new_list_pop = new_list.pop()
+print(new_list_pop)
+print(new_list)
+
+#7
+list_invert = [100,200,300,400,500]
+print(list_invert.reverse())
+
+#8
+list_order = [3,1,4,2,5]
+print(list_order.sort())
+
+#9
+list_1 = [1,2,3]
+list_2 = [4,5,6]
+print(list_1 + list_2)
+
+#10
+sublist = list2[1:3]
+print(sublist)
