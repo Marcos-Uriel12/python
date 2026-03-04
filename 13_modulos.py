e534739855bc336e3eaf073f5512eb7ec46617c4
Marcos <marcosurielmachuca49@gmail.com>
2026-03-04 00:52:29 -0300

    13_modulos.py

---
+++ 13_modulos.py
@@ -0,0 +1,29 @@
+#1
+import calculator
+print(calculator.calculadora(10,5,"suma"))
+print(calculator.calculadora(10,5,"resta"))
+print(calculator.calculadora(10,5,"multiplicacion"))
+print(calculator.calculadora(10,5,"division"))
+
+#2
+import convert
+print(convert.grados(40,"C"))
+print(convert.grados(104,"F"))
+
+#3
+from Estudiantes import mostrar_estudiantes, Estudiantes
+mostrar_estudiantes(Estudiantes)
+
+#4
+from Areas import cuadrado, circulo
+print(cuadrado(5))  
+print(circulo(3))
+
+#5
+import Argumentos
+print(Argumentos.args(1,2,3,10))
+
+#6
+import car
+print(car.car("Toyota", "Corolla", 2020))
+
